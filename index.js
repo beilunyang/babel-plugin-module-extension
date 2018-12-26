49e432018a0857d30833705164c4e49f9682263a
beilunyang <786220806@qq.com>
2018-12-26 17:58:47 +0800

    feat: 转换导入模块的扩展名

---
+++ index.js
@@ -0,0 +1,35 @@
+/*
+ * @Author: beilunyang
+ * @Date: 2018-12-26 14:24:26
+ * @Last Modified by: beilunyang
+ * @Last Modified time: 2018-12-26 17:20:43
+ */
+
+const transformExt = (rules, node) => {
+  const originExts = Object.keys(rules);
+  for (const originExt of originExts) {
+    const originExtRegx = new RegExp(`.${originExt}$`);
+    const replaceValue = rules[originExt] ? `.${rules[originExt]}` : '';
+    const value = node.value.replace(originExtRegx, replaceValue);
+    if (value !== node.value) {
+      node.value = value;
+      break;
+    }
+  }
+};
+
+module.exports = () => {
+  return {
+    visitor: {
+      ImportDeclaration(path, state) {
+        transformExt(state.opts, path.node.source);
+      },
+      CallExpression(path, state) {
+        if (path.node.callee.name === "require") {
+          const requireArg = path.node.arguments[0];
+          transformExt(state.opts, requireArg);
+        }
+      }
+    }
+  };
+};
